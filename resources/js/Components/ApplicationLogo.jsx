8d1cf3f9eabd757e446fcc8806719ca24765a382
Gareth Watt <djgwatt@gmail.com>
2025-05-10 11:18:39 +0900

    fix/correctly combine classnamne in app logo
    fixes the image height problem

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,22 +1,24 @@
 import { Link, usePage } from "@inertiajs/react";
+import { cn } from "@/shadcn";
 
 export default function ApplicationLogo({ imgClass = "", textClass = "" }) {
-    const { appName, globalSettings } = usePage().props;
-    return (
+	const { appName, globalSettings } = usePage().props;
+	return (
 		<Link
-		href="/"
-		className="flex items-center gap-2 text-lg font-semibold md:text-base"
-	>
-		{globalSettings.general.app_logo
-			? <img
-				loading="lazy"
-				decoding="async"
-				src={globalSettings.general.app_logo}
-				alt={globalSettings.general.app_logo || appName}
-				className={["h-12", imgClass]}
-			/>
-			: <h1 className={textClass}>{globalSettings.general.app_name || appName}</h1>
-		}
-	</Link>
-    );
+			href="/"
+			className="flex items-center gap-2 text-lg font-semibold md:text-base"
+		>
+			{globalSettings.general.app_logo
+				? <img
+					loading="lazy"
+					decoding="async"
+					src={globalSettings.general.app_logo}
+					alt={globalSettings.general.app_logo || appName}
+					className={cn("h-12", imgClass)}
+
+				/>
+				: <h1 className={textClass}>{globalSettings.general.app_name || appName}</h1>
+			}
+		</Link>
+	);
 }
